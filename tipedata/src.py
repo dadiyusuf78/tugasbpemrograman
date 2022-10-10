61cd88bd31190265d5348eef8c5178aea4915b48
dadiyusuf78 <dadiyusuf78@student.esaunggul.ac.id>
2022-10-10 12:43:02 +0700

    tugas

---
+++ tipedata/src.py
@@ -0,0 +1,14 @@
+print(type(h))
+#Boolean
+i = True
+j = False
+print(i,j)
+print(type(i))
+print(type(j))
+# Contoh
+list_f = [1,2,3]
+print (list_f[0])
+tuple_f = 1,2,3
+print (tuple_f[0])
+set_f = {1,2,3}
+print (set_f)
---
+++ variabel/src.py
@@ -0,0 +1,25 @@
+# variabel
+a ="Dadi Yusuf"
+def func():
+    a = "dadi"
+    print ("selamat " + a)
+func()
+print(a)
+# Definisi
+def tambah():
+    a = 5
+    b = 3
+    c = a+b
+    print(c)
+tambah()
+# Definisi Parameter
+def data(nama,nim):
+    print(f"nama saya {nama} dan nim saya {nim}")
+data("Dadi","123")
+# Contoh
+def data(sisi):
+    return(sisi*sisi)
+data(2)
+def total(alas,tinggi):
+    return(0.5*alas*tinggi)
+data(2,5)
