5f0fcf1590f2890856aa2ad506bde5cd60c0a8ed
dadiyusuf78 <dadiyusuf78@student.esaunggul.ac.id>
2022-10-10 12:46:59 +0700

    tugas

--- tipedata/src.py
+++ tipedata/src.py
@@ -1,3 +1,36 @@
+# String
+a = "hello"
+print (a)
+print (type(a))
+#Int
+b = 15
+print (b)
+print (type(b))
+# Float
+c = 10.5
+print (c)
+print (type(c))
+# Tuple
+d = 20,5
+print (d)
+print(type(d))
+# Complex
+e = 1j
+print (e)
+print(type(e))
+z = complex (5-9j)
+print (z)
+# List
+f = ["a","b","c"]
+print (f)
+print (type(f))
+# Set
+g ={"a","b","c"}
+print (g)
+print (type(g))
+# Frozenset
+h = frozenset({1,2,3})
+print(h)
 print(type(h))
 #Boolean
 i = True
